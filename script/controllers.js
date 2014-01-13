996abaa5be19160e22ff25b90ea675ca0c80b7f3
gucheen <guchengf@gmail.com>
2014-01-13 23:09:31 +0800

    adjust view of search bar and order bar,add test api of douban

--- script/controllers.js
+++ script/controllers.js
@@ -1,10 +1,13 @@
 var bookshelfControllers = angular.module('bookshelfControllers',[]);
-
 bookshelfControllers.controller('bookListCtrl',['$scope','$http',
     function($scope,$http){
         $http.get('books/books.json').success(function(data){
-        $scope.books = data;
-    });
+            $scope.books = data;
+        });
+
+        /*$.ajax({url:'http://api.douban.com/v2/book/6801488?apikey=0d0ddd1b987ff58827299752c97fe098',dataType:'jsonp'}).done(function(data){
+            console.log(data['author']+','+data['id']);
+        });*/
 
     $scope.orderProp = '-year';
 }]);
