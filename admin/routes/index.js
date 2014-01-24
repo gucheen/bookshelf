23c8e2c9e019a6927842e2de039cde450017ac79
gucheen <guchengf@gmail.com>
2014-01-24 18:21:47 +0800

    fix bugs

--- admin/routes/index.js
+++ admin/routes/index.js
@@ -1,9 +1,13 @@
 var fs = require('fs');
 
 exports.index = function(req, res){
-  fs.readFile('../books/books.json','utf-8',function(err,data){
+  fs.readFile('books/books.json','utf-8',function(err,data){
+  	if(err){
+  		console.log(err);
+  	}else{
       var resource = JSON.parse(data);
       res.render('index', { title: 'Admin Panel', description: 'This is admin panel of the bookshelf.', list: resource });
+    }
   });
 };
 
